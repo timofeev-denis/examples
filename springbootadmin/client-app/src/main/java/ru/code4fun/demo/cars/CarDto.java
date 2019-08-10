eec1a4fcf6624bd499c3bcb2e596b1c5a7e66322
Denis Timofeev <nakolenke@gmail.com>
2019-08-10 18:03:19 +0300

    Add Spring Boot Admin example

---
+++ springbootadmin/client-app/src/main/java/ru/code4fun/demo/cars/CarDto.java
@@ -0,0 +1,16 @@
+package ru.code4fun.demo.cars;
+
+/**
+ * Created by: Denis Timofeev
+ * Date: 24.07.2019
+ */
+
+public class CarDto {
+    private String brand;
+    private String model;
+
+    public CarDto(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+}
---
+++ springbootadmin/client-app/src/main/java/ru/code4fun/demo/cars/CarsController.java
@@ -0,0 +1,45 @@
+package ru.code4fun.demo.cars;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static org.springframework.http.ResponseEntity.ok;
+
+/**
+ * Created by: Denis Timofeev
+ * Date: 24.07.2019
+ */
+
+@RefreshScope
+@RestController
+@RequestMapping("/api/v1/rest/cars")
+public class CarsController {
+
+    @Value("${cars.limit}")
+    private Integer limit;
+
+    /**
+     * Returns a list of cars. List size is limited to the value of <b>cars.limit</b> property.
+     *
+     * @return A list of cars
+     */
+    @GetMapping
+    public ResponseEntity<List> getCars() {
+        List<CarDto> cars = Arrays.asList(
+                new CarDto("Ford", "Mustang"),
+                new CarDto("Ford", "F-1"),
+                new CarDto("Chevrolet", "Camaro"),
+                new CarDto("BMW", "X5"));
+        return ok(cars.stream()
+                .limit(limit)
+                .collect(toList()));
+    }
+}
