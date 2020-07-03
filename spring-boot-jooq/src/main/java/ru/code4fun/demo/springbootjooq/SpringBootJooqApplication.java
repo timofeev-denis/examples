379e3b951c6d739786b676a97f0cae8d7dde5992
Denis Timofeev <nakolenke@gmail.com>
2020-07-03 21:00:46 +0300

    Создание проекта

---
+++ spring-boot-jooq/src/main/java/ru/code4fun/demo/springbootjooq/SpringBootJooqApplication.java
@@ -0,0 +1,13 @@
+package ru.code4fun.demo.springbootjooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJooqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJooqApplication.class, args);
+    }
+
+}
---
+++ spring-boot-jooq/src/test/java/ru/code4fun/demo/springbootjooq/SpringBootJooqApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.code4fun.demo.springbootjooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJooqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
