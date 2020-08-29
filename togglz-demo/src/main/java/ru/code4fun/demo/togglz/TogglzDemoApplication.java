1b4863c2ad51d13916aa636f6752d3d0a35412ea
Денис Тимофеев <dtimofeev@stream.ru>
2020-08-29 14:05:09 +0300

    Create project

---
+++ togglz-demo/src/main/java/ru/code4fun/demo/togglz/TogglzDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.code4fun.demo.togglz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogglzDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TogglzDemoApplication.class, args);
+    }
+
+}
