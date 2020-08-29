7615362321b98bb7911e530103fb9dad1e2ecdc7
Denis Timofeev <nakolenke@gmail.com>
2020-08-29 14:05:09 +0300

    Create project

---
+++ togglz-demo/src/main/java/ru/code4fun/demo/togglz/controller/HelloController.java
@@ -0,0 +1,13 @@
+package ru.code4fun.demo.togglz.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Message A";
+    }
+}
---
+++ togglz-demo/src/test/java/ru/code4fun/demo/togglz/TogglzDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.code4fun.demo.togglz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogglzDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
