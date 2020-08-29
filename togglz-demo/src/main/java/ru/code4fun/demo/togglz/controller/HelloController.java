9161a97d0c351b9c054109a037ed15aa14e2cf27
Денис Тимофеев <dtimofeev@stream.ru>
2020-08-29 14:38:35 +0300

    Add feature

--- togglz-demo/src/main/java/ru/code4fun/demo/togglz/controller/HelloController.java
+++ togglz-demo/src/main/java/ru/code4fun/demo/togglz/controller/HelloController.java
@@ -2,12 +2,27 @@ package ru.code4fun.demo.togglz.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.togglz.core.manager.FeatureManager;
+import org.togglz.core.util.NamedFeature;
 
 @RestController
 public class HelloController {
 
+    private FeatureManager featureManager;
+
+    public HelloController(FeatureManager featureManager) {
+        this.featureManager = featureManager;
+    }
+
     @GetMapping("/hello")
     public String sayHello() {
-        return "Message A";
+        return getMessage();
+    }
+
+    private String getMessage() {
+        if (featureManager.isActive(new NamedFeature("MESSAGE_A"))) {
+            return "Message A";
+        }
+        return "Default message";
     }
 }
