caf4e72bd041b68142e1aca53b66be1dfc01397d
Denis Timofeev <nakolenke@gmail.com>
2020-08-29 14:45:11 +0300

    Type-safe feature name

--- togglz-demo/src/main/java/ru/code4fun/demo/togglz/controller/HelloController.java
+++ togglz-demo/src/main/java/ru/code4fun/demo/togglz/controller/HelloController.java
@@ -3,7 +3,8 @@ package ru.code4fun.demo.togglz.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.togglz.core.manager.FeatureManager;
-import org.togglz.core.util.NamedFeature;
+
+import static ru.code4fun.demo.togglz.config.ApplicationFeatures.MESSAGE_A;
 
 @RestController
 public class HelloController {
@@ -20,7 +21,7 @@ public class HelloController {
     }
 
     private String getMessage() {
-        if (featureManager.isActive(new NamedFeature("MESSAGE_A"))) {
+        if (featureManager.isActive(MESSAGE_A)) {
             return "Message A";
         }
         return "Default message";
