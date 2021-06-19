ccc529f9c0691b11ab8cae7c622b1a9747d672ff
Denis Timofeev <nakolenke@gmail.com>
2021-06-19 11:53:35 +0300

    Create project

---
+++ jwt/src/test/java/ru/code4fun/demo/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.code4fun.demo.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
