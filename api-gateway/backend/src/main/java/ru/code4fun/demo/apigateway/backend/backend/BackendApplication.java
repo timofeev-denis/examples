8f47b75443ac697c5c594b66bfd636941006f0e5
Denis Timofeev <nakolenke@gmail.com>
2022-04-17 21:17:38 +0300

    Создание модуля backend

---
+++ api-gateway/backend/src/main/java/ru/code4fun/demo/apigateway/backend/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package ru.code4fun.demo.apigateway.backend.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
---
+++ api-gateway/backend/src/test/java/ru/code4fun/demo/apigateway/backend/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.code4fun.demo.apigateway.backend.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
