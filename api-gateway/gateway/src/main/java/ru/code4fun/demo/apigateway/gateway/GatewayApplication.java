68cdc6519615800405b7132784be67176f7a9bde
Denis Timofeev <nakolenke@gmail.com>
2022-04-17 21:32:04 +0300

    Создание модуля gateway

---
+++ api-gateway/gateway/src/main/java/ru/code4fun/demo/apigateway/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.code4fun.demo.apigateway.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
---
+++ api-gateway/gateway/src/test/java/ru/code4fun/demo/apigateway/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.code4fun.demo.apigateway.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
