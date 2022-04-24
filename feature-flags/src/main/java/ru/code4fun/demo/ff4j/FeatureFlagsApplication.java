9780120c38d3b958f76e0d3ceba6bf11d7ed071b
Denis Timofeev <nakolenke@gmail.com>
2022-04-24 17:18:13 +0300

    Create project

---
+++ feature-flags/src/main/java/ru/code4fun/demo/ff4j/FeatureFlagsApplication.java
@@ -0,0 +1,13 @@
+package ru.code4fun.demo.ff4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureFlagsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeatureFlagsApplication.class, args);
+    }
+
+}
---
+++ feature-flags/src/test/java/ru/code4fun/demo/ff4j/FeatureFlagsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.code4fun.demo.ff4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureFlagsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
