9ae294c348c0363b2157c27f24562a91a1b4d73e
Denis Timofeev <nakolenke@gmail.com>
2022-05-09 16:11:05 +0300

    Create movieinfo-app module

---
+++ moviecatalog/movieinfo-app/src/main/java/ru/code4fun/demo/moviecatalog/movieinfoapp/MovieinfoAppApplication.java
@@ -0,0 +1,13 @@
+package ru.code4fun.demo.moviecatalog.movieinfoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieinfoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieinfoAppApplication.class, args);
+    }
+
+}
---
+++ moviecatalog/movieinfo-app/src/test/java/ru/code4fun/demo/moviecatalog/movieinfoapp/MovieinfoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.code4fun.demo.moviecatalog.movieinfoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieinfoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
