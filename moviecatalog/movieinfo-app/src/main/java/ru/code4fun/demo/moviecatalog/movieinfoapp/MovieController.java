192a02788eeabb23e0e9570459cdd7888208d378
Denis Timofeev <nakolenke@gmail.com>
2022-05-09 16:29:43 +0300

    Add "/movies/{id}" method

--- moviecatalog/movieinfo-app/src/main/java/ru/code4fun/demo/moviecatalog/movieinfoapp/MovieController.java
+++ moviecatalog/movieinfo-app/src/main/java/ru/code4fun/demo/moviecatalog/movieinfoapp/MovieController.java
@@ -1,6 +1,8 @@
 package ru.code4fun.demo.moviecatalog.movieinfoapp;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +13,22 @@ import java.util.List;
 @RequestMapping("/movies")
 public class MovieController {
 
+    private List<MovieDto> movies = List.of(
+            new MovieDto(1L, "The Terminator", "Action", LocalDate.of(1984, 10, 26)),
+            new MovieDto(2L, "The Dark Knight", "Action", LocalDate.of(2008, 7, 14)),
+            new MovieDto(3L, "The Lord of the Rings: The Return of the King", "Adventure", LocalDate.of(2003, 12, 1)),
+            new MovieDto(4L, "Pulp Fiction", "Crime", LocalDate.of(1994, 5, 21)),
+            new MovieDto(5L, "The Godfather", "Drama", LocalDate.of(1972, 3, 14)));
+
     @GetMapping("")
     public List<MovieDto> findAll() {
-        return List.of(
-                new MovieDto("The Terminator", "Action", LocalDate.of(1984, 10, 26)),
-                new MovieDto("The Dark Knight", "Action", LocalDate.of(2008, 7, 14)),
-                new MovieDto("The Lord of the Rings: The Return of the King", "Adventure", LocalDate.of(2003, 12, 1)),
-                new MovieDto("Pulp Fiction", "Crime", LocalDate.of(1994, 5, 21)),
-                new MovieDto("The Godfather", "Drama", LocalDate.of(1972, 3, 14)));
+        return this.movies;
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieDto> findById(@PathVariable Long id) {
+        return ResponseEntity.of(this.movies.stream()
+                .filter(m -> m.getId().equals(id))
+                .findFirst());
+    }
 }
--- moviecatalog/movieinfo-app/src/main/java/ru/code4fun/demo/moviecatalog/movieinfoapp/MovieDto.java
+++ moviecatalog/movieinfo-app/src/main/java/ru/code4fun/demo/moviecatalog/movieinfoapp/MovieDto.java
@@ -3,14 +3,24 @@ package ru.code4fun.demo.moviecatalog.movieinfoapp;
 import java.time.LocalDate;
 
 public class MovieDto {
+    private Long id;
     private String title;
     private String genre;
     private LocalDate releasedAt;
 
-    public MovieDto(String title, String description, LocalDate releasedAt) {
+    public MovieDto(Long id, String title, String description, LocalDate releasedAt) {
+        this.id = id;
         this.title = title;
         this.genre = description;
         this.releasedAt = releasedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
